0765ad884cfe03bf0e12e85e9767214126d55777
BespalchukDY <founder.10@mail.ru>
2020-05-17 20:18:15 +0500

    Выполнил задания к 1 уроку

---
+++ Lesson_1/main.cpp
@@ -0,0 +1,13 @@
+//Задания выполнил Беспальчук Денис
+
+#include "mainwindow.hpp"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
